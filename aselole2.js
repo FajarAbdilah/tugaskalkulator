82a5ed50aaa63f56c2741d891f4ca064af405765
tiarasopiyanti <118708571+tiarasopiyanti@users.noreply.github.com>
2022-12-08 20:05:11 -0800

    Update aselole2.js

--- aselole2.js
+++ aselole2.js
@@ -13,3 +13,12 @@ var bil1= input.questionInt("masukan angka =")
 var bil2= input.questionInt("masukan angka =")
 var hasil = bil1 + bil2
 console.log(hasil)
+if (input2=="*"){
+var bil1= input.question("masukan angka =")
+var bil2= input.question("masukan angka =")
+console.log(bil1*bil2)
+}else if(input2 =="/"){
+var bil1= input.questionInt("masukan angka =")
+var bil2= input.questionInt("masukan angka =")
+var hasil = bil1 / bil2
+console.log(hasil)
